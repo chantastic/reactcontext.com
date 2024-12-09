c976cfd716fc628653c12a5a6be09eadd1bbcd28
Michael Chan <mijoch@gmail.com>
2024-12-08 19:50:51 -0800

    Add astro-expressive-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import astroExpressiveCode from 'astro-expressive-code'
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [astroExpressiveCode()]
+});
